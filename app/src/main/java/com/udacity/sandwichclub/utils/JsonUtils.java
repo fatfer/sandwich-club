1c1e13031f591b919741a0c0833fb92f6e8ea484
Fernando Guerra <pher.gm@gmail.com>
2018-02-25 20:13:19 +0100

    Extract constants

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -11,17 +11,25 @@ import java.util.List;
 
 public class JsonUtils {
 
+    private static final String JSON_NAME_KEY = "name";
+    private static final String JSON_MAIN_NAME_KEY = "mainName";
+    private static final String JSON_ALSO_KNOWN_AS_KEY = "alsoKnownAs";
+    private static final String JSON_PLACE_OF_ORIGIN_KEY = "placeOfOrigin";
+    private static final String JSON_DESCRIPTION_KEY = "description";
+    private static final String JSON_IMAGE_KEY = "image";
+    private static final String JSON_INGREDIENTS_KEY = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) throws JSONException {
         Sandwich sandwich = new Sandwich();
 
         JSONObject jsonObject = new JSONObject(json);
-        JSONObject name = jsonObject.getJSONObject("name");
-        sandwich.setMainName(name.getString("mainName"));
-        sandwich.setAlsoKnownAs(jsonArrayToList(name.getJSONArray("alsoKnownAs")));
-        sandwich.setPlaceOfOrigin(jsonObject.getString("placeOfOrigin"));
-        sandwich.setDescription(jsonObject.getString("description"));
-        sandwich.setImage(jsonObject.getString("image"));
-        sandwich.setIngredients(jsonArrayToList(jsonObject.getJSONArray("ingredients")));
+        JSONObject name = jsonObject.getJSONObject(JSON_NAME_KEY);
+        sandwich.setMainName(name.getString(JSON_MAIN_NAME_KEY));
+        sandwich.setAlsoKnownAs(jsonArrayToList(name.getJSONArray(JSON_ALSO_KNOWN_AS_KEY)));
+        sandwich.setPlaceOfOrigin(jsonObject.getString(JSON_PLACE_OF_ORIGIN_KEY));
+        sandwich.setDescription(jsonObject.getString(JSON_DESCRIPTION_KEY));
+        sandwich.setImage(jsonObject.getString(JSON_IMAGE_KEY));
+        sandwich.setIngredients(jsonArrayToList(jsonObject.getJSONArray(JSON_INGREDIENTS_KEY)));
 
         return sandwich;
     }
